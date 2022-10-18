c92837302c06a50f0012009c1f5b6406ef00b242
Khanya Sotashe <s223186570@mandela.ac.za>
2022-10-18 11:05:20 +0200

    Khanya change

---
+++ 2022 Final Project/Property.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace _2022_Final_Project
+{
+    partial class Property
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProperty = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProperty)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProperty
+            // 
+            this.dgvProperty.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProperty.Location = new System.Drawing.Point(12, 288);
+            this.dgvProperty.Name = "dgvProperty";
+            this.dgvProperty.RowHeadersWidth = 51;
+            this.dgvProperty.RowTemplate.Height = 24;
+            this.dgvProperty.Size = new System.Drawing.Size(776, 150);
+            this.dgvProperty.TabIndex = 0;
+            // 
+            // Property
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProperty);
+            this.Name = "Property";
+            this.Text = "Property";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProperty)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProperty;
+    }
+}
